228a80fb27859448a46500fa9fbef50062fb557a
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-01 00:06:43 +0530

    add: added sytles for top container for player controls

--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import styles from "../styles/PlayerControls.module.scss";
+import {
+  playerControlSync,
+  playerControlLoop,
+  playerControlLeft,
+  playerControlPlay,
+  playerControlRight,
+  playerControlLoopRight,
+  playerControlSettings,
+} from "../assets/index";
 
 const PlayerControls: React.FC = () => {
   return (
     <div className={styles.PlayerControlsContainer}>
-      <section className={styles.TopContainer}></section>
+      <section className={styles.TopContainer}>
+        <div className={styles.LeftControls}>
+          <img src={playerControlSync} alt="sync-icon" />
+          <img src={playerControlLoop} alt="loop-icon" />
+        </div>
+        <div className={styles.CenterControls}>
+          <img src={playerControlLeft} alt="left-icon" />
+          <img src={playerControlPlay} alt="play-icon" />
+          <img src={playerControlRight} alt="right-icon" />
+        </div>
+        <div className={styles.RightControls}>
+          <img src={playerControlLoopRight} alt="another-loop-icon" />
+          <img src={playerControlSettings} alt="settings-icon" />
+        </div>
+      </section>
       <div className={styles.BottomContainer}></div>
     </div>
   );
