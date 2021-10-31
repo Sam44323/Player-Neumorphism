88fe426d6588b7ceba87bc19a4f7a2e7709fa4f4
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-31 22:04:15 +0530

    add: added style files for components

--- src/components/AlbumContainer.tsx
+++ src/components/AlbumContainer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from '../styles/AlbumContainer.module.scss'
 
 const AlbumContainer: React.FC = () => {
   return <></>
--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from '../styles/PlayerControls.module.scss';
 
 const PlayerControls: React.FC = () => {
   return <></>
