aaead04578e764cc97e14aecd4971d47b15f0c13
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-31 23:57:01 +0530

    add: added required assets for player control container

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,23 @@
 import share from "./share.svg";
 import addToPlaylist from "./add-to-playlist.svg";
 import likeSong from "./like-song.png";
+import playerControlSync from "./player-control-sync.png";
+import playerControlLoop from "./player-control-loop.png";
+import playerControlLeft from "./player-control-left.png";
+import playerControlPlay from "./player-control-play.png";
+import playerControlRight from "./player-control-right.png";
+import playerControlLoopRight from "./player-control-loop-another.png";
+import playerControlSettings from "./player-control-settings.png";
 
-export { share, addToPlaylist, likeSong };
+export {
+  share,
+  addToPlaylist,
+  likeSong,
+  playerControlSync,
+  playerControlLoop,
+  playerControlLeft,
+  playerControlPlay,
+  playerControlRight,
+  playerControlLoopRight,
+  playerControlSettings,
+};
--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "../styles/PlayerControls.module.scss";
 
 const PlayerControls: React.FC = () => {
-  return <div className={styles.PlayerControlsContainer}></div>;
+  return (
+    <div className={styles.PlayerControlsContainer}>
+      <section className={styles.TopContainer}></section>
+      <div className={styles.BottomContainer}></div>
+    </div>
+  );
 };
 
 export default PlayerControls;
