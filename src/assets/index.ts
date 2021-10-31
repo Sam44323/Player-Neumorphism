e5fd6ae9d95e8c8972e78d717c3ec92258fd3e6d
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-31 22:35:34 +0530

    update: updated the assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,5 @@
 import share from "./share.svg";
 import addToPlaylist from "./add-to-playlist.svg";
-import likeSong from "./like-song.svg";
+import likeSong from "./like-song.png";
 
 export { share, addToPlaylist, likeSong };
--- src/components/AlbumContainer.tsx
+++ src/components/AlbumContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../styles/AlbumContainer.module.scss";
+import { likeSong, addToPlaylist, share } from "../assets/index";
 
 const AlbumContainer: React.FC = () => {
   return (
@@ -10,7 +11,21 @@ const AlbumContainer: React.FC = () => {
           alt="album-cover"
         />
       </section>
-      <section className={styles.AlbumDetailsControlsSection}></section>
+      <section className={styles.AlbumDetailsControlsSection}>
+        <p>Now Playing</p>
+        <section className={styles.AlbumDetails}>
+          <p>Purple Haze</p>
+          <p>Jimi Hendrix</p>
+          <p>Woodstock</p>
+        </section>
+        <section className={styles.AlbumControls}>
+          {[likeSong, addToPlaylist, share].map((icon, index) => (
+            <div key={index} className={styles.ControlsContainer}>
+              <img src={icon} alt="icon" />
+            </div>
+          ))}
+        </section>
+      </section>
     </div>
   );
 };
