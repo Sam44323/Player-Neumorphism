7093fa5b05e5430ed2305c2784165e8337e7d63e
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-31 22:12:27 +0530

    add: added custom configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.scss';
+import AlbumContainer from './components/AlbumContainer';
+import PlayerControls from './components/PlayerControls';
 
 const  App: React.FC = () => {
   return (
     <div className="App">
-
+      <AlbumContainer />
     </div>
   );
 }
--- src/components/AlbumContainer.tsx
+++ src/components/AlbumContainer.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import styles from '../styles/AlbumContainer.module.scss'
+import React from "react";
+import styles from "../styles/AlbumContainer.module.scss";
 
 const AlbumContainer: React.FC = () => {
-  return <></>
-}
+  return (
+    <div className={styles.AlbumContainer}>
+      <section className={styles.AlbumImageSection}>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/61waYHWxV%2BL._SL1200_.jpg"
+          alt="album-cover"
+        />
+      </section>
+      <section className={styles.AlbumDetailsControlsSection}></section>
+    </div>
+  );
+};
 
 export default AlbumContainer;
