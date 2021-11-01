c396598d642d1ac31be2de58b95e64c39018ccb6
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-01 15:16:45 +0530

    update: updated the range styles

--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from "../styles/PlayerControls.module.scss";
-import Slider, { SliderThumb } from "@mui/material/Slider";
-import { styled } from "@mui/material/styles";
 
 import {
   playerControlSync,
@@ -11,48 +9,10 @@ import {
   playerControlRight,
   playerControlLoopRight,
   playerControlSettings,
+  sliderThumb,
 } from "../assets/index";
 
 const PlayerControls: React.FC = () => {
-  const PrettoSlider = styled(Slider)({
-    color: "#52af77",
-    height: 8,
-    "& .MuiSlider-track": {
-      border: "none",
-    },
-    "& .MuiSlider-thumb": {
-      height: 24,
-      width: 24,
-      backgroundColor: "#fff",
-      border: "2px solid currentColor",
-      "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
-        boxShadow: "inherit",
-      },
-      "&:before": {
-        display: "none",
-      },
-    },
-    "& .MuiSlider-valueLabel": {
-      lineHeight: 1.2,
-      fontSize: 12,
-      background: "unset",
-      padding: 0,
-      width: 32,
-      height: 32,
-      borderRadius: "50% 50% 50% 0",
-      backgroundColor: "#52af77",
-      transformOrigin: "bottom left",
-      transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
-      "&:before": { display: "none" },
-      "&.MuiSlider-valueLabelOpen": {
-        transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
-      },
-      "& > *": {
-        transform: "rotate(45deg)",
-      },
-    },
-  });
-
   return (
     <div className={styles.PlayerControlsContainer}>
       <section className={styles.TopContainer}>
@@ -71,7 +31,7 @@ const PlayerControls: React.FC = () => {
         </div>
       </section>
       <div className={styles.BottomContainer}>
-        <PrettoSlider valueLabelDisplay="auto" defaultValue={20} />
+        <input type="range" />
       </div>
     </div>
   );
