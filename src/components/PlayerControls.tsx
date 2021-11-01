b2d7f722e18859add642898902ce04903156a6cb
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-01 14:37:00 +0530

    add: added mui support emotions for react

--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styles from "../styles/PlayerControls.module.scss";
+import Slider, { SliderThumb } from "@mui/material/Slider";
+import { styled } from "@mui/material/styles";
+
 import {
   playerControlSync,
   playerControlLoop,
@@ -11,6 +14,45 @@ import {
 } from "../assets/index";
 
 const PlayerControls: React.FC = () => {
+  const PrettoSlider = styled(Slider)({
+    color: "#52af77",
+    height: 8,
+    "& .MuiSlider-track": {
+      border: "none",
+    },
+    "& .MuiSlider-thumb": {
+      height: 24,
+      width: 24,
+      backgroundColor: "#fff",
+      border: "2px solid currentColor",
+      "&:focus, &:hover, &.Mui-active, &.Mui-focusVisible": {
+        boxShadow: "inherit",
+      },
+      "&:before": {
+        display: "none",
+      },
+    },
+    "& .MuiSlider-valueLabel": {
+      lineHeight: 1.2,
+      fontSize: 12,
+      background: "unset",
+      padding: 0,
+      width: 32,
+      height: 32,
+      borderRadius: "50% 50% 50% 0",
+      backgroundColor: "#52af77",
+      transformOrigin: "bottom left",
+      transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
+      "&:before": { display: "none" },
+      "&.MuiSlider-valueLabelOpen": {
+        transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
+      },
+      "& > *": {
+        transform: "rotate(45deg)",
+      },
+    },
+  });
+
   return (
     <div className={styles.PlayerControlsContainer}>
       <section className={styles.TopContainer}>
@@ -28,7 +70,13 @@ const PlayerControls: React.FC = () => {
           <img src={playerControlSettings} alt="settings-icon" />
         </div>
       </section>
-      <div className={styles.BottomContainer}></div>
+      <div className={styles.BottomContainer}>
+        <PrettoSlider
+          valueLabelDisplay="auto"
+          aria-label="pretto slider"
+          defaultValue={20}
+        />
+      </div>
     </div>
   );
 };
