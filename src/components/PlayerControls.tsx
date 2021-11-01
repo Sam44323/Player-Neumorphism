97290332f9ce207ea0094403acd08aa8d5458648
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-02 00:17:05 +0530

    add: added sections for bottom container

--- src/components/PlayerControls.tsx
+++ src/components/PlayerControls.tsx
@@ -36,50 +36,52 @@ const PlayerControls: React.FC = () => {
         </div>
       </section>
       <div className={styles.BottomContainer}>
-        <Slider aria-label="slider-ex-1" defaultValue={0}>
-          <SliderTrack
-            style={{
-              height: "10px",
-              borderRadius: "17.5px",
-              background: "#DEDDE3",
-              padding: "9px 0",
-              boxShadow:
-                "-3px -3px 5px #FFFFFF, 3px 3px 5px rgba(0, 0, 0, 0.05), inset 3px 3px 5px rgba(0, 0, 0, 0.05), inset -3px -3px 5px #FFFFFF",
-            }}
-          >
-            <SliderFilledTrack
+        <section>
+          <Slider aria-label="slider-ex-1" defaultValue={0}>
+            <SliderTrack
               style={{
-                width: "100%",
-                margin: "0 4px",
-                borderRadius: "31px",
-                background:
-                  "linear-gradient(327.56deg, #5D24D6 19.23%, #7E74ED 81.76%)",
+                height: "10px",
+                borderRadius: "17.5px",
+                background: "#DEDDE3",
+                padding: "9px 0",
                 boxShadow:
-                  "0px 2px 3px rgba(43, 72, 180, 0.4), inset 1px 1px 3px rgba(255, 255, 255, 0.4)",
+                  "-3px -3px 5px #FFFFFF, 3px 3px 5px rgba(0, 0, 0, 0.05), inset 3px 3px 5px rgba(0, 0, 0, 0.05), inset -3px -3px 5px #FFFFFF",
               }}
-            />
-          </SliderTrack>
-          <SliderThumb
-            boxSize={6}
-            style={{
-              border: "none",
-              outline: "none",
-            }}
-          >
-            <img
-              src={sliderThumb}
-              alt="thumb"
+            >
+              <SliderFilledTrack
+                style={{
+                  width: "100%",
+                  margin: "0 4px",
+                  borderRadius: "31px",
+                  background:
+                    "linear-gradient(327.56deg, #5D24D6 19.23%, #7E74ED 81.76%)",
+                  boxShadow:
+                    "0px 2px 3px rgba(43, 72, 180, 0.4), inset 1px 1px 3px rgba(255, 255, 255, 0.4)",
+                }}
+              />
+            </SliderTrack>
+            <SliderThumb
+              boxSize={6}
               style={{
-                maxWidth: "none",
-                marginTop: "14px",
+                border: "none",
+                outline: "none",
               }}
-            />
-          </SliderThumb>
-        </Slider>
-        <div className={styles.TimerSection}>
+            >
+              <img
+                src={sliderThumb}
+                alt="thumb"
+                style={{
+                  maxWidth: "none",
+                  marginTop: "14px",
+                }}
+              />
+            </SliderThumb>
+          </Slider>
+        </section>
+        <section className={styles.TimerSection}>
           <p>0 : 00 : 00</p>
           <p>4 : 01 : 30</p>
-        </div>
+        </section>
       </div>
     </div>
   );
